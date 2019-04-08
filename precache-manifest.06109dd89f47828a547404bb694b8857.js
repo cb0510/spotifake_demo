e9f7796c03ee2f2e21dfbc552064841c67022697
cb0510 <34616485+cb0510@users.noreply.github.com>
2019-04-09 00:22:57 +0700

    Initial Commit
    Initial Commit

---
+++ precache-manifest.06109dd89f47828a547404bb694b8857.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b247528119edb290cdf64390d18e565",
+    "url": "/spotifake_demo/static/media/temp.8b247528.gif"
+  },
+  {
+    "revision": "c26794782f66436e3a54f41648db067f",
+    "url": "/spotifake_demo/static/media/noimage.c2679478.png"
+  },
+  {
+    "revision": "17eb45df63498296d9e79ac70770c32e",
+    "url": "/spotifake_demo/static/media/cover-small.17eb45df.jpeg"
+  },
+  {
+    "revision": "b818d523d8560f33bcf5d6628f0ffa73",
+    "url": "/spotifake_demo/static/media/Spotify_Icon_RGB_Black.b818d523.png"
+  },
+  {
+    "revision": "24369df7012c430be68e",
+    "url": "/spotifake_demo/static/js/runtime~main.24369df7.js"
+  },
+  {
+    "revision": "4f01886bd266a5355982",
+    "url": "/spotifake_demo/static/js/main.4f01886b.chunk.js"
+  },
+  {
+    "revision": "79e2bed6001f2229a241",
+    "url": "/spotifake_demo/static/js/2.79e2bed6.chunk.js"
+  },
+  {
+    "revision": "4f01886bd266a5355982",
+    "url": "/spotifake_demo/static/css/main.ccd13b1b.chunk.css"
+  },
+  {
+    "revision": "79e2bed6001f2229a241",
+    "url": "/spotifake_demo/static/css/2.e9a251a9.chunk.css"
+  },
+  {
+    "revision": "3a2e37d872d00707a5ecfea92a1cdc5a",
+    "url": "/spotifake_demo/index.html"
+  }
+];
